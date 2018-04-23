82dec9e31754e788a4a310f8930203229269f00c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Application.xaml.vb
@@ -0,0 +1,26 @@
+﻿' Developer Express Code Central Example:
+' How to delete grid rows via the DeleteFocusedRow grid command
+' 
+' The following example demonstrates how to add a Delete item to the grid cell's
+' context menu.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E1791
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+
+Namespace GridCommands_DeleteFocusedRow
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+	End Class
+End Namespace
