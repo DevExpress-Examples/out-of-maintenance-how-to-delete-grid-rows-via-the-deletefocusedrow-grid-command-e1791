0c6f42e80ee5135f84096d49e25c41edbcb7fb41
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to delete grid rows via the DeleteFocusedRow grid command
+
+
+<p>The following example demonstrates how to add a <strong>Delete</strong> item to the grid cell's context menu.</p>
+
+<br/>
+
+
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Documents
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Bars
+
+Namespace GridCommands_DeleteFocusedRow
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New AccountList().GetData()
+		End Sub
+
+	End Class
+
+	#Region "Account classes"
+	Public Class AccountList
+		Public Function GetData() As List(Of Account)
+			Return CreateAccounts()
+		End Function
+		Private Function CreateAccounts() As List(Of Account)
+			Dim list As New List(Of Account)()
+			list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = DateTime.Today})
+			list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New DateTime(2009, 5, 10)})
+			list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New DateTime(2008, 12, 22)})
+			list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = DateTime.Today})
+			list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New DateTime(2008, 10, 18)})
+			Return list
+		End Function
+	End Class
+
+	Public Class Account
+		Private privateUserName As String
+		Public Property UserName() As String
+			Get
+				Return privateUserName
+			End Get
+			Set(ByVal value As String)
+				privateUserName = value
+			End Set
+		End Property
+		Private privateRegistrationDate As DateTime
+		Public Property RegistrationDate() As DateTime
+			Get
+				Return privateRegistrationDate
+			End Get
+			Set(ByVal value As DateTime)
+				privateRegistrationDate = value
+			End Set
+		End Property
+	End Class
+	#End Region
+End Namespace
