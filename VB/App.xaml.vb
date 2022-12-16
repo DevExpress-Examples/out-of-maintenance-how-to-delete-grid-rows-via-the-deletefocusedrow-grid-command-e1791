8437df4eac8f71c71478a3c89dad3b89d17cea3c
DevExpressExampleBot <fakeEmail>
2022-12-17 01:12:31 +0400

    Source auto update [skip ci]

--- VB/App.xaml.vb
+++ VB/App.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to delete grid rows via the DeleteFocusedRow grid command
 ' 
 ' The following example demonstrates how to add a Delete item to the grid cell's
@@ -6,21 +6,15 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E1791
-
-
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
 Imports System.Windows
 
 Namespace GridCommands_DeleteFocusedRow
-	''' <summary>
-	''' Interaction logic for App.xaml
-	''' </summary>
-	Partial Public Class App
-		Inherits Application
-	End Class
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
 End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to delete grid rows via the DeleteFocusedRow grid command
 ' 
 ' The following example demonstrates how to add a Delete item to the grid cell's
@@ -6,46 +6,45 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E1791
-
-
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Documents
-Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Bars
 
 Namespace GridCommands_DeleteFocusedRow
 
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = New AccountList().GetData()
-		End Sub
+    Public Partial Class Window1
+        Inherits Window
 
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = New AccountList().GetData()
+        End Sub
+    End Class
 
-	#Region "Account classes"
-	Public Class AccountList
-		Public Function GetData() As List(Of Account)
-			Return CreateAccounts()
-		End Function
-		Private Function CreateAccounts() As List(Of Account)
-			Dim list As New List(Of Account)()
-			list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = DateTime.Today})
-			list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New DateTime(2009, 5, 10)})
-			list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New DateTime(2008, 12, 22)})
-			list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = DateTime.Today})
-			list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New DateTime(2008, 10, 18)})
-			Return list
-		End Function
-	End Class
+'#Region "Account classes"
+    Public Class AccountList
 
-	Public Class Account
-		Public Property UserName() As String
-		Public Property RegistrationDate() As DateTime
-	End Class
-	#End Region
+        Public Function GetData() As List(Of Account)
+            Return CreateAccounts()
+        End Function
+
+        Private Function CreateAccounts() As List(Of Account)
+            Dim list As List(Of Account) = New List(Of Account)()
+            list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = Date.Today})
+            list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New DateTime(2009, 5, 10)})
+            list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New DateTime(2008, 12, 22)})
+            list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = Date.Today})
+            list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New DateTime(2008, 10, 18)})
+            Return list
+        End Function
+    End Class
+
+    Public Class Account
+
+        Public Property UserName As String
+
+        Public Property RegistrationDate As Date
+    End Class
+'#End Region
 End Namespace
